cc5efce4863ed2ca9b156ab2fe0fb3bbfc24b548
Joshua Ryder <joshua.ryder@outlook.com>
2020-05-26 13:55:36 +0200

    Add secure gRPC examples

---
+++ src/TopSwagCode.GRPC.Server.Secure/Services/WeatherForecastService.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
+
+namespace TopSwagCode.GRPC.Server.Secure
+{
+
+    [Authorize]
+    public class WeatherForecastService : WeatherForecasts.WeatherForecastsBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastService> _logger;
+
+        public WeatherForecastService(ILogger<WeatherForecastService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task GetWeatherStream(Empty request, IServerStreamWriter<WeatherData> responseStream, ServerCallContext context)
+        {
+            var rng = new Random();
+            var now = DateTime.UtcNow;
+
+            var i = 0;
+            while (!context.CancellationToken.IsCancellationRequested && i < 20)
+            {
+                await Task.Delay(500); // Gotta look busy
+
+                var forecast = new WeatherData
+                {
+                    DateTimeStamp = Timestamp.FromDateTime(now.AddDays(i++)),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                };
+
+                _logger.LogInformation("Sending WeatherData response");
+
+                if (!context.CancellationToken.IsCancellationRequested) // Extra guard. Cancellation might have been done while work was been done.
+                {
+                    await responseStream.WriteAsync(forecast);
+                }
+                else
+                {
+                    _logger.LogWarning("Stream has been cancelled by the client.");
+                }
+            }
+        }
+    }
+}
---
+++ src/TopSwagCode.GRPC.Server.Secure/Startup.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace TopSwagCode.GRPC.Server.Secure
+{
+    public class Startup
+    {
+        public static string Key => "kV8aOW32PfSf+0KDAQZGhLL9pYRnifQp3ESj1D9RMv8=";
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddGrpc();
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy(JwtBearerDefaults.AuthenticationScheme, policy =>
+                {
+                    policy.AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme);
+                    policy.RequireClaim(ClaimTypes.Name);
+                });
+            });
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters =
+                        new TokenValidationParameters
+                        {
+                            ValidateAudience = false,
+                            ValidateIssuer = false,
+                            ValidateActor = false,
+                            ValidateLifetime = true,
+                            IssuerSigningKey = SecurityKey
+                        };
+                });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGrpcService<WeatherForecastService>();
+
+                endpoints.MapGet("/generateJwtToken", context =>
+                {
+                    return context.Response.WriteAsync(GenerateJwtToken(context.Request.Query["name"]));
+                });
+            });
+        }
+        private string GenerateJwtToken(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new InvalidOperationException("Name is not specified.");
+            }
+
+            var claims = new[] { new Claim(ClaimTypes.Name, name) };
+            var credentials = new SigningCredentials(SecurityKey, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken("ExampleServer", "ExampleClients", claims, expires: DateTime.Now.AddSeconds(60), signingCredentials: credentials);
+            return JwtTokenHandler.WriteToken(token);
+        }
+
+        private readonly JwtSecurityTokenHandler JwtTokenHandler = new JwtSecurityTokenHandler();
+        private readonly SymmetricSecurityKey SecurityKey = new SymmetricSecurityKey(Guid.NewGuid().ToByteArray());
+    }
+}
